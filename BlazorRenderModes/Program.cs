373fcb2864883a8794a79e1c525627e0bd7044c5
KofinasApostolos <a.kofinas@informlykos.com>
2024-03-05 09:40:44 +0200

    Add hardcoded test api call

--- BlazorRenderModes/Program.cs
+++ BlazorRenderModes/Program.cs
@@ -11,13 +11,23 @@ builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
 // your TMDB Read Access key must be in the server's secrets.json, e.g.:
-// "TMDBKey": "your-API-key-here"
-string? tmdbKey = builder.Configuration["TMDBKey"];
+//"TMDBKey": "your-API-key-here"
+//string? tmdbKey = builder.Configuration["TMDBKey"];
+
+//builder.Services.AddScoped(sp => {
+//    var client = new HttpClient();
+//    client.BaseAddress = new("https://api.themoviedb.org/3/");
+//    client.DefaultRequestHeaders.Authorization = new("Bearer", tmdbKey);
+//    return client;
+//});
+
+string tmdbApiKey = "b2b90f9e1a4b1b08f3e57cd4db272957zzzzzz";
+string tmdbReadAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmI5MGY5ZTFhNGIxYjA4ZjNlNTdjZDRkYjI3Mjk1NyIsInN1YiI6IjY1ZTVjYjBhNDRhNDI0MDE2MzExOGM4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dEPRMt7FykKb0iKPfDc5TJduRzQMxxkP5EthF4JXwkA";
 
 builder.Services.AddScoped(sp => {
     var client = new HttpClient();
     client.BaseAddress = new("https://api.themoviedb.org/3/");
-    client.DefaultRequestHeaders.Authorization = new("Bearer", tmdbKey);
+    client.DefaultRequestHeaders.Authorization = new("Bearer", tmdbApiKey);
     return client;
 });
 
